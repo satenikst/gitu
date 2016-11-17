6a7bf87646493629edea5bb9aa16c14b21d4fcf8
unknown <satenikst@gmail.com>
2016-11-17 15:43:50 +0400

    chngs

--- public/js/controller.js
+++ public/js/controller.js
@@ -10,6 +10,9 @@ app.config(function($routeProvider){
 	.when('/SettingUpARepository', {
 		templateUrl:'repo.html'
 	})
+	.when('/SavingChanges', {
+		templateUrl:'save.html'
+	})
 	.otherwise({
         template: "'<br><br><br><br><br><h1 style='color:red;text-align:center'>404</h1><br><h2 style='color:red;text-align:center'>oops...something went wrong</h2><br><h3 style='color:green;text-align:center'>but don’t give up on us and try starting again...</h3><a href='#/' style='margin-left:35%;'>Go to home page</a>"
     });
