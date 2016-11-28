f670e47c1c58c12bcf2e3a917b99e6c5ebb6c00b
unknown <satenikst@gmail.com>
2016-11-28 22:48:05 +0400

    changes

--- public/js/controller.js
+++ public/js/controller.js
@@ -22,6 +22,9 @@ app.config(function($routeProvider){
 	.when('/RewritingHistory', {
 		templateUrl:'rewrite.html'
 	})
+	.when('/Syncing', {
+		templateUrl:'syncing.html'
+	})
 	.otherwise({
         template: "'<br><br><br><br><br><h1 style='color:red;text-align:center'>404</h1><br><h2 style='color:red;text-align:center'>oops...something went wrong</h2><br><h3 style='color:green;text-align:center'>but don’t give up on us and try starting again...</h3><a href='#/' style='margin-left:35%;'>Go to home page</a>"
     });
